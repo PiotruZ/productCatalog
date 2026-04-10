d0b3eebd12d624346ac73cdba1f68de0cbad04d3
cf-yharbar <yaroslav.harbar@cloudfide.com>
2026-04-10 12:53:09 +0200

    feat: add producer analytics

---
+++ src/main/java/com/example/productcatalog/analytics/LogService.java
@@ -0,0 +1,12 @@
+package com.example.productcatalog.analytics;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class LogService {
+
+    public void logMetrics(Long producerId, List<String> metrics) {
+    }
+}
